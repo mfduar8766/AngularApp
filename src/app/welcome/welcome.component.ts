52004bc653799cfe07165be6ba21454d9be0d8d8
Matheus Duarte <mfduar8766@gmail.com>
2018-10-31 22:59:07 -0400

    Added images and links to them. Createted products component IE:images.

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Tile } from '../Interfaces/Tile.interface';
 
 @Component({
   selector: 'app-welcome',
@@ -6,10 +7,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent implements OnInit {
+  tiles: Tile[] = [
+    {
+      text: 'One',
+      columns: 2,
+      rows: 2,
+      class: 'one-tile'
+    },
+    {
+      text: 'Two',
+      columns: 1,
+      rows: 1,
+      class: 'two-tile',
+    },
+    {
+      text: 'Three',
+      columns: 1,
+      rows: 2,
+      class: 'three-tile',
+    },
+    {
+      text: 'Four',
+      columns: 1,
+      rows: 2,
+      class: 'four-tile',
+    },
+    {
+      text: 'Five',
+      columns: 2,
+      rows: 1,
+      class: 'five-tile',
+    },
+    {
+      text: 'Six',
+      columns: 1,
+      rows: 1,
+      class: 'six-tile',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
